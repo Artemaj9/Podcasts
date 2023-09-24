52fbd34540fe5538f29cf1464d816c817ea80589
stacy177 <75451219+stacy177@users.noreply.github.com>
2023-09-24 19:04:33 +0300

    initial commit

---
+++ Challenge3/Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Анастасия on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
