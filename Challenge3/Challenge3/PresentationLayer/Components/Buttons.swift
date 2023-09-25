a3c906ae3d104da58fed1f709d8b9b91fe115de8
n1K1Ta00 <aleks.avers12@mail.ru>
2023-09-25 16:55:48 +0300

    button

--- Challenge3/Challenge3/PresentationLayer/Components/Buttons.swift
+++ Challenge3/Challenge3/PresentationLayer/Components/Buttons.swift
@@ -5,13 +5,42 @@
 import SwiftUI
 
 struct Buttons: View {
+    
+    @State var select = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            RoundedButton(action: {}, title: "Войти", font: .headline, width: 326, height: 57, isReverse: false,foregroundColor: .white,background: .blue,cornerRadius: 50,padding: 10)
+           
+        }
     }
 }
 
 struct Buttons_Previews: PreviewProvider {
     static var previews: some View {
         Buttons()
+    }
+}
+struct RoundedButton: View {
+    var title: String
+    var font: Font = .system(size: 16)
+    var isReverse: Bool = false
+    var foregroundColor: Color = .white
+    var background: Color = .blue
+    var cornerRadius: Double = 10
+    var padding: Double = 16
+    
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(font)
+                .padding(padding)
+                .background(background)
+                .foregroundColor(foregroundColor)
+                .cornerRadius(cornerRadius)
+
+        }
     }
 }
