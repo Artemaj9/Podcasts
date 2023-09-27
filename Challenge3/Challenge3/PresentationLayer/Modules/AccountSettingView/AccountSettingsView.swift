b8a61e96a49d44d101c417543bd350e80c85fb0f
n1K1Ta00 <aleks.avers12@mail.ru>
2023-09-27 20:42:12 +0300

    screen Account Setting
    no merge

--- Challenge3/Challenge3/PresentationLayer/Modules/AccountSettingView/AccountSettingsView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/AccountSettingView/AccountSettingsView.swift
@@ -8,9 +8,67 @@ struct AccountSettingsView: View, ItemView {
     var listener: CustomNavigationContainer?
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+           NavigationView {
+               VStack(alignment: .center) {
+                   
+                   Image(Images.DefaultView.avatar.rawValue)
+                       .overlay(
+                        Image(Images.Icon.edit.rawValue)
+                            .offset(x: 35, y: 35)
+                                         )
+
+                   LoginTextField(inputText: "",
+                                  title: "First name",
+                                  placeHolder: "First name",
+                                  withHideOption: false,
+                                  withBorder: true,
+                                  cornerRadius: 24,
+                                  backgroundColor: .white)
+                   
+                   LoginTextField(inputText: "",
+                                  title: " Last name",
+                                  placeHolder: "Last name",
+                                  withHideOption: false,
+                                  withBorder: true,
+                                  cornerRadius: 24,
+                                  backgroundColor: .white)
+                   
+                   LoginTextField(inputText: "",
+                                  title: "E-mail ",
+                                  placeHolder: "E-mail",
+                                  withHideOption: false,
+                                  withBorder: true,
+                                  cornerRadius: 24,
+                                  backgroundColor: .white)
+                   
+                   Text("Date of Birth")
+                   HStack {
+                       Spacer()
+                       
+                       Text("24 februry 1996")
+                       
+                       Spacer()
+                       
+                   }
+                   .padding(.vertical)
+                   .overlay(
+                        Capsule()
+                            .stroke(Color.blue, lineWidth: 2)
+                   )
+                   .padding()
+                   
+                   CustomButton(title: "Save Changes",buttonType: .filledGray) {
+                       //
+                   }
+               }
+               .makeCustomNavBar {
+                   NavigationBars(atView: .accountSetting) {
+                    //
+                   }
+               }
+           }
+       }
+   }
 
 struct AccountSettingsView_Previews: PreviewProvider {
     static var previews: some View {
