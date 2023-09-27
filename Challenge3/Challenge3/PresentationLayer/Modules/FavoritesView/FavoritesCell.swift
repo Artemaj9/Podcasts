79ff57bfe42f627e9d1a965c8819311d92d122cf
stacy177 <75451219+stacy177@users.noreply.github.com>
2023-09-27 17:20:32 +0300

    added Favorites View

---
+++ Challenge3/Challenge3/PresentationLayer/Modules/FavoritesView/FavoritesCell.swift
@@ -0,0 +1,59 @@
+//
+//  FavoritesCell.swift//
+
+import SwiftUI
+
+struct FavoritesCell: View {
+    let image: String?
+    let firstText: String?
+    let secondText: String?
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Pallete.OtherLight.peach)
+            .frame(width: 120, height: 160)
+            .overlay(
+                VStack(spacing: 10) {
+                    if let image {
+                        Image(image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(10)
+                            .padding(.top, 30)
+                    } else {
+                        Text("")
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(10)
+                            .background(Pallete.Gray.forPhotoCells)
+                            .padding(.top, 30)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 6) {
+                        if let firstText {
+                            Text(firstText)
+                                .font(.headline)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Pallete.BlackWhite.black)
+                        }
+                        
+                        if let secondText {
+                            Text(secondText)
+                                .font(.subheadline)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Pallete.Gray.forText)
+                        }
+                    }
+                    .padding(.bottom, 20)
+                }
+            )
+    }
+}
+
+struct FavoritesCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesCell(image: nil, firstText: "first", secondText: "second")
+    }
+}
--- Challenge3/Challenge3/PresentationLayer/Modules/FavoritesView/FavoritesView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/FavoritesView/FavoritesView.swift
@@ -5,10 +5,76 @@
 import SwiftUI
 
 struct FavoritesView: View, ItemView {
+    
+    //MARK: - Internal Properties
+    
     var listener: CustomNavigationContainer?
+    
+    //MARK: - Mock data
+    
+    let data: [(image: String?, firstText: String?, secondText: String?)] = [
+        (image: nil, firstText: "Text1", secondText: "Description1"),
+        (image: "image2", firstText: "Text2", secondText: "Description2"),
+        (image: "image2", firstText: "Text3", secondText: "Description3"),
+        (image: "image2", firstText: "Text4", secondText: "Description4")
+    ]
+    
+    let playlists: [Playlist] = [
+        Playlist(mainTitle: "Tuhan mengapa dia berbeda", secondTitle: "15 Eps"),
+        Playlist(mainTitle: "Another Playlist", secondTitle: "10 Eps")
+    ]
 
+    //MARK: - View's Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NavigationBars(atView: .playlist) { }
+            .padding(.bottom)
+            
+            HStack {
+                Text(Localizable.Favorite.favorites)
+                
+                Spacer ()
+                
+                Button {
+                    
+                } label: {
+                    Text(Localizable.Favorite.seeAll)
+                        .foregroundColor(Pallete.Gray.forText)
+                }
+            }
+            .padding(.horizontal)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(data, id: \.firstText) { item in
+                        FavoritesCell(image: item.image, firstText: item.firstText, secondText: item.secondText)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            
+            Text(Localizable.Favorite.yourPlaylist)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.top, .leading])
+
+            ScrollView() {
+                BlankWideCell(
+                    mainTitle: "Create playlist", //нужно решить проблему локализации
+                    cellMode: .new
+                )
+                .padding([.top, .leading])
+                
+                ForEach(playlists, id: \.mainTitle) { playlist in
+                    BlankWideCell(
+                        mainTitle: playlist.mainTitle,
+                        secondTitle: playlist.secondTitle,
+                        cellMode: .normal
+                    )
+                    .padding([.top, .leading])
+                }
+            }
+        }
     }
 }
 
