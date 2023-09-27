cb08194728087255610f6d3c9bb2522d1b9b1b83
O4ErtO <vekhin200631@gmail.com>
2023-09-27 21:54:57 +0300

    added

--- Challenge3/Challenge3/PresentationLayer/Modules/AutorizationView/AutorizationView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/AutorizationView/AutorizationView.swift
@@ -4,16 +4,44 @@
 
 import SwiftUI
 
-struct AutorizationView: ItemView {
-    var listener: CustomNavigationContainer?
-
+struct AutorizationView: View {
+    @StateObject var viewModel: AuthorizarionViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            LoginTextField(inputText: viewModel.login, title: "Введите логин", placeHolder: "Логин", withHideOption: false, withBorder: false, cornerRadius: 12)
+            Spacer()
+            LoginTextField(inputText: viewModel.password, isSecure: false, title: "Введите пароль", placeHolder: "Пароль", withHideOption: true, withBorder: false, cornerRadius: 12)
+            Spacer()
+            CustomButton(title: "Войти", cornerRadius: 100, buttonType: .filledBlue, action: {})
+                .frame(height: 62)
+            Spacer()
+            HStack{
+                Rectangle()
+                    .frame(width: 62, height: 1)
+                Text("Or continue with")
+                Rectangle()
+                    .frame(width: 62, height: 1)
+            }.foregroundColor(Pallete.Gray.forNext)
+            Spacer()
+            CustomButton(title: "Sign in with Google", buttonType: .outGoogle, action: {} )
+                
+            Spacer()
+            HStack{
+                Spacer()
+                Text("У вас еще нет аккаунта?")
+                        .font(.system(size: 12))
+                StringButton(title: "Зарегистрироваться", font: (.system(size: 12)), foregroundColor: .green, action: {})
+                Spacer()
+            }
+            
+        }
     }
 }
 
-struct AutorizationView_Previews: PreviewProvider {
-    static var previews: some View {
-        AutorizationView()
-    }
+#Preview {
+    AutorizationView(viewModel: AuthorizarionViewModel())
 }
+
+
