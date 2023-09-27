23ba7997dd33848c61b8007c212a8a64483cf95d
O4ErtO <vekhin200631@gmail.com>
2023-09-28 00:08:13 +0300

    added

--- Challenge3/Challenge3/PresentationLayer/Modules/CreateView/CreateWithEmail/CreateWithEmailView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/CreateView/CreateWithEmail/CreateWithEmailView.swift
@@ -5,15 +5,47 @@
 import SwiftUI
 
 struct CreateWithEmailView: View, ItemView {
+    
+    @StateObject var viewModel: CreateWithEmailViewModel
+    
     var listener: CustomNavigationContainer?
-
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            NavigationBars(atView: .signUp, leadingButtonAction: {}, trailingButtonAction: {})
+            
+            VStack{
+                
+                
+                Spacer()
+                CustomLabel(labelText: "Complete your account", additionalText: "for continue", labelStyle: .create, epsText: "")
+                Spacer()
+                LoginTextField(inputText: viewModel.firstName, title: "First name", placeHolder: "Enter your first name", withHideOption: false, withBorder: false, cornerRadius: 24, backgroundColor: Pallete.Gray.forTextFields)
+                Spacer()
+                LoginTextField(inputText: viewModel.lastName, title: "Last name", placeHolder: "Enter your last name", withHideOption: false, withBorder: false, cornerRadius: 24, backgroundColor: Pallete.Gray.forTextFields)
+                Spacer()
+                LoginTextField(inputText: viewModel.email, title: "E-mail", placeHolder: "Enter your email", withHideOption: false, withBorder: false, cornerRadius: 24, backgroundColor: Pallete.Gray.forTextFields)
+                Spacer()
+                LoginTextField(inputText: viewModel.password, title: "Password", placeHolder: "Enter your password", withHideOption: true, withBorder: false, cornerRadius: 24, backgroundColor: Pallete.Gray.forTextFields)
+                Spacer()
+                LoginTextField(inputText: viewModel.confpassword, title: "Confirm password", placeHolder: "Confirm your password", withHideOption: true, withBorder: false, cornerRadius: 24, backgroundColor: Pallete.Gray.forTextFields)
+                Spacer()
+                CustomButton(title: "Sign up", font: (.system(size: 16)), buttonType: .filledBlue, action: {})
+                Spacer()
+                HStack{
+                    Text("Already have an account?")
+                    
+                    StringButton(title: "Login", font: (.system(size: 16)), foregroundColor: .green, action: {})
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
 struct CreatetWithEmailView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateWithEmailView()
+        CreateWithEmailView(viewModel: CreateWithEmailViewModel())
     }
 }
