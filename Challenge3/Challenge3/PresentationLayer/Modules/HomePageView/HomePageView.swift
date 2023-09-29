64bac706e1ca7731d9fdcd1f39b769079a8eb9df
kupriyanovNik <cucuprianov@icloud.com>
2023-09-29 21:36:54 +0300

    fixed UI + fixed CodeStyle + added Localizable

--- Challenge3/Challenge3/PresentationLayer/Modules/HomePageView/HomePageView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/HomePageView/HomePageView.swift
@@ -6,8 +6,20 @@ import SwiftUI
 
 struct HomePageView: View, ItemView {
 
+    // MARK: - Internal Properties
+
     var listener: CustomNavigationContainer?
+
+    // MARK: - Property Wrapper
+
     @State private var selectedCategory: Int = 0
+
+    // MARK: - Private Properties
+
+    var strings = Localizable.HomePage.self
+    var categoriesStrings = Localizable.HomePage.Categories.self
+
+    // MARK: - Private View Properties
 
     private var titleRow: some View {
         HStack {
@@ -21,12 +33,13 @@ struct HomePageView: View, ItemView {
         }
         .padding(.horizontal, 32)
     }
+
     private var categoryAndSeeAllRow: some View {
         HStack {
-            Text("Category")
+            Text(strings.category)
                 .font(.system(size: 16, weight: .semibold))
             Spacer()
-            Text("See all")
+            Text(strings.seeAll)
                 .foregroundColor(Pallete.Gray.forText)
                 .font(.system(size: 16))
                 .onTapGesture {
@@ -36,6 +49,7 @@ struct HomePageView: View, ItemView {
         .padding(.horizontal, 32)
         .padding(.top)
     }
+
     private var categoryItemsRow: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
@@ -53,24 +67,45 @@ struct HomePageView: View, ItemView {
             }
         }
     }
+
     private var categorySelectionView: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
-                CategoryTabView(selectedCategory: $selectedCategory, data: ["🔥 Popular", "Recent", "Music", "Design"])
+                CategoryTabView(selectedCategory: $selectedCategory, data: [
+                    categoriesStrings.popular,
+                    categoriesStrings.recent,
+                    categoriesStrings.music,
+                    categoriesStrings.design
+                ])
             }
         }
     }
+
     private var categorizedRows: some View {
-        VStack(spacing: 16) {
-            ForEach(0..<30) { index in
-                FilledWideCell(mainLeft: "Ngobam", mainRight: "Gofar Hilman", secondLeft: "Music & Fun", secondRight: "23 Eps", iconMode: .like)
+        Group {
+            switch selectedCategory {
+            case 0:
+                VStack(spacing: 16) {
+                    ForEach(0..<30) { index in
+                        FilledWideCell(mainLeft: "Ngobam", mainRight: "Gofar Hilman", secondLeft: "Music & Fun", secondRight: "23 Eps", iconMode: .like)
+                    }
+                }
+                .padding(.horizontal, 32)
+            case 1:
+                Text("recent")
+            case 2:
+                Text("music")
+            case 3:
+                Text("Design")
+            default:
+                Text("unknown")
             }
         }
-        .padding(.horizontal, 32)
     }
 
     var body: some View {
         ScrollView(showsIndicators: false) {
+
             VStack(spacing: 16) {
                 categoryAndSeeAllRow
                 VStack(spacing: 24) {
@@ -79,11 +114,15 @@ struct HomePageView: View, ItemView {
                 }
                 categorizedRows
             }
+
         }
         .makeCustomNavBar {
             titleRow
         }
     }
+
+    // MARK: - Functions
+
     @ViewBuilder private func createCategoryCell() -> some View {
         VStack {
             Spacer()
