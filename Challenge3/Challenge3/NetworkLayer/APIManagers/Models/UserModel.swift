bffcc2cdc0d33cae76e009ff4223fa34e6df76d2
Kabrel <kshutov666@gmail.com>
2023-09-29 22:20:13 +0300

    added UserModel

--- Challenge3/Challenge3/NetworkLayer/APIManagers/Models/UserModel.swift
+++ Challenge3/Challenge3/NetworkLayer/APIManagers/Models/UserModel.swift
@@ -4,19 +4,19 @@
 
 import Firebase
 
-//struct User: Identifiable {
-//    let id: String
-//    let firstName: String
-//    let lastName: String
-//    let email: String
-//    let profileImageUrl: String
-//    
-//    init(data: [String: Any]) {
-//        self.id = data["uid"] as? String ?? ""
-//        self.firstName = data["firstName"] as? String ?? ""
-//        self.lastName = data["lastName"] as? String ?? ""
-//        self.email = data["email"] as? String ?? ""
-//        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
-//    }
-//}
+struct User: Identifiable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let profileImageUrl: String
+    
+    init(data: [String: Any]) {
+        self.id = data["uid"] as? String ?? ""
+        self.firstName = data["firstName"] as? String ?? ""
+        self.lastName = data["lastName"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
+    }
+}
 
