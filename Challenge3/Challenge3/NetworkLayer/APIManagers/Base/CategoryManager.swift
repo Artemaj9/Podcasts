b2475c6d1f3da0ca83109f352a667a49931941c1
Kabrel <kshutov666@gmail.com>
2023-10-01 16:36:24 +0300

    added CategoryManager

---
+++ Challenge3/Challenge3/NetworkLayer/APIManagers/Base/CategoryManager.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryManager.swift
+//
+
+import Foundation
+import PodcastIndexKit
+
+struct PodcastCategory: Codable, Hashable, Sendable {
+    public let id: Int?
+    public let name: String?
+}
+
+final class CategoryManager: ObservableObject {
+    @Published var apiError = ""
+    
+    func getCategoryList(debug: Bool = false) async -> [PodcastCategory]? {
+        do {
+            let result = try await PodcastIndexKit().categoriesService.list(pretty: debug)
+            return result.feeds as? [PodcastCategory]
+        }
+        catch {
+            return nil
+        }
+    }
+}
