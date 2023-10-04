04349bb5973de1b0bd94134a5a780e2cdf76bc4e
Artem Moiseev <moiseev@physics.msu.ru>
2023-10-04 18:20:07 +0300

    add rough player view with slider control

--- Challenge3/Challenge3/PresentationLayer/Modules/NowPlayingView/NowPlayingView.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/NowPlayingView/NowPlayingView.swift
@@ -3,15 +3,139 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct NowPlayingView: View, ItemView {
     var listener: CustomNavigationContainer?
+    
+    @StateObject var vm  = NowPlayingViewModel()
+    @StateObject var player = Player(avPlayer: AVPlayer(url: URL(string: "https://traffic.libsyn.com/saskapriest/homily-bishopmark-20231001.mp3")!))
+    
+    @State var opacity: Double = 1
+    @State var currentBackground = 0
+    
+    
+    @State var startingOffsetX: CGFloat = UIScreen.main.bounds.width * 0
+    @State var endingOffsetX: CGFloat = 0
+    
+    let array = ["avatar","avatar","avatar","avatar", "avatar","avatar","avatar","avatar"]
+    
+var body: some View {
+        ZStack {
+            GeometryReader {
+                geo in
+                Image(array[currentBackground])
+                    .resizable()
+                    .scaledToFill()
+                    .scaleEffect(2)
+                    .blur(radius: 50)
+                    .animation(.easeIn(duration: 1), value: currentBackground)
+            }
+            .ignoresSafeArea()
+            
+            VStack {
+                ScrollViewReader { scrollProxy in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .center, spacing: 10) {
+                            Color.clear
+                                .frame(width: (UIScreen.main.bounds.size.width/8))
+                            
+                            ForEach(Array(0..<7), id: \.self) { id in
+                                GeometryReader { geometry in
+                                    ZStack(alignment: .center) {
+                                        Image(String(array[id]))
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            .padding(.horizontal, 4)
+                                            .frame(width: UIScreen.main.bounds.size.width * 0.7)
+                                    }
+                                    .frame(width: UIScreen.main.bounds.size.width * 0.7)
+                                    .opacity(vm.getScrollOpacity(geometry: geometry))
+                                    .scaleEffect(y:vm.getScrollOpacity(geometry: geometry))
+                                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                                        .onChanged{ value in
+                                            withAnimation(.spring()) {
+                                                vm.currentDragOffsetX = value.translation.width
+                                            }
+                                        }
+                                        .onEnded({_ in
+                                            currentBackground = vm.scrollto(id: id, offsetX:  vm.currentDragOffsetX)
+                                            withAnimation {
+                                                scrollProxy.scrollTo(vm.scrollto(id: id, offsetX: vm.currentDragOffsetX), anchor: .center)
+                                                vm.scrollFlag = vm.scrollFlag(id: id, offsetX: vm.currentDragOffsetX)
+                                                if vm.scrollFlag {
+                                                    vm.setUpTimer()
+                                                } else {
+                                                    vm.currentDragOffsetX = 0
+                                                }
+                                            }
+                                            
+                                        }))
+                                }
+                                .frame(width: 280, height: 240)
+                            }
 
-    var body: some View {
-        Text("Hello, World!")
+                            Color.clear
+                                .frame(width: (UIScreen.main.bounds.size.width - 70) / 2.0)
+                        }
+                        .padding(.bottom, -20)
+                    }
+                }
+                VStack {
+                    Text("Track")
+                        .font(.title)
+                        .foregroundColor(Pallete.Other.deepPurpleText)
+                    Text("Author")
+                        .font(.body)
+                        .foregroundColor(Pallete.Other.deepPurpleText)
+                }
+                .offset(y: -40)
+                if self.player.itemDuration > 0 {
+                    VStack(alignment: .trailing) {
+                        Slider(value: self.$player.displayTime, in: (0...self.player.itemDuration), onEditingChanged: {
+                            (scrubStarted) in
+                            if scrubStarted {
+                                self.player.scrubState = .scrubStarted
+                            } else {
+                                self.player.scrubState = .scrubEnded(self.player.displayTime)
+                            }
+                        })
+                        HStack {
+                            Text(vm.durationFormatter.string(from: self.player.displayTime) ?? "")
+                            Text("/")
+                            Text(vm.durationFormatter.string(from: self.player.itemDuration) ?? "")
+                        }
+                        .foregroundColor(Pallete.Other.deepPurpleText)
+                        .opacity(0.8)
+                    }
+                    .padding()
+                }
+                
+                Button(action: {
+                    switch self.player.timeControlStatus {
+                    case .paused:
+                        self.player.play()
+                    case .waitingToPlayAtSpecifiedRate:
+                        self.player.pause()
+                    case .playing:
+                        self.player.pause()
+                    @unknown default:
+                        fatalError()
+                    }
+                }) {
+                    Image(systemName: self.player.timeControlStatus == .paused ? "play" : "pause")
+                        .imageScale(.large)
+                        .scaleEffect(1.5)
+                        .frame(width: 64, height: 64)
+                }
+                    .padding(.vertical, 30)
+                
+            }
+        }
     }
 }
-
+  
 struct NowPlayingView_Previews: PreviewProvider {
     static var previews: some View {
         NowPlayingView()
--- Challenge3/Challenge3/PresentationLayer/Modules/NowPlayingView/NowPlayingViewModel.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/NowPlayingView/NowPlayingViewModel.swift
@@ -2,6 +2,112 @@
 //  NowPlayingViewModel.swift
 //
 
-import Foundation
+import SwiftUI
+import Combine
 
-final class NowPlayingViewModel: ObservableObject {}
+final class NowPlayingViewModel: ObservableObject {
+    
+    @Published var currentDragOffsetX:CGFloat = 0
+    @Published var count: CGFloat = 0
+    @Published var scrollFlag = false
+    
+    var durationFormatter: DateComponentsFormatter {
+
+        let durationFormatter = DateComponentsFormatter()
+        durationFormatter.allowedUnits = [.minute, .second]
+        durationFormatter.unitsStyle = .positional
+        durationFormatter.zeroFormattingBehavior = .pad
+
+        return durationFormatter
+    }
+    
+    // Cancellables
+    
+  var cancellables = Set<AnyCancellable>()
+        
+        func getScrollOpacity(geometry: GeometryProxy) -> Double {
+            let maxX = UIScreen.main.bounds.width
+            
+            // Координата верхней границы контейнера
+            let currentX = geometry.frame(in: .global).midX
+
+            let opacity: Double
+            
+            //координата, с которой начинает убывать прозрачнотсь
+            let xInitial = maxX
+            let xInitial2 = 0*maxX
+            
+            // координата, на которой прозрачность становится нулевой
+            let xFinal = 1.7*maxX
+            let xFinal2 = -0.7*maxX
+            
+            // угловой коэффициент линейной зависимости
+            let k = 1 / (xInitial - xFinal)
+            let kTop = 1 / (xInitial2 - xFinal2)
+            
+            // свободный коэффициент в линейной зависимости
+            let b = -k*xFinal
+            let bTop = -kTop*xFinal2
+            
+            if currentX < xInitial && currentX > xInitial2 {
+                opacity = 1
+            } else if currentX >= xInitial {
+                opacity = k * currentX + b
+            } else  {
+                opacity = kTop * currentX + bTop
+            }
+                
+            return opacity
+        }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
+    }
+    
+    func scrollto(id: Int, offsetX: CGFloat) -> Int {
+        var idToScroll: Int
+        if offsetX > 100 {
+            idToScroll = id != 0 ? id - 1 : id
+        } else if abs(offsetX) <= 100  {
+                idToScroll = id
+        } else {
+            idToScroll = id + 1
+        }
+        return idToScroll
+    }
+    
+    func scrollFlag(id: Int, offsetX: CGFloat) -> Bool {
+        var flag: Bool
+        if offsetX > 100 && id != 0 {
+            flag = true
+        } else if abs(offsetX) <= 100  {
+                flag = false
+        } else {
+            flag = true
+        }
+        return flag
+    }
+    
+    
+    func setUpTimer() {
+        Timer
+            .publish(every: 0.01, on: .main, in: .common)
+            .autoconnect()
+            .sink { [unowned self] _ in
+                count +=  1
+                if count == 5 {
+                    currentDragOffsetX = 0
+                }
+                if count == 25 {
+                    currentDragOffsetX = 0
+                    count = 0
+                    for item in cancellables {
+                        item.cancel()
+                    }
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
