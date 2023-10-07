134c8e22bd131621b9bf275d3fe5bfbb776cf1be
Artem Moiseev <moiseev@physics.msu.ru>
2023-10-07 03:12:35 +0300

    Cleaned SearchResultViewModel

--- Challenge3/Challenge3/PresentationLayer/Modules/SearchView/SearchResult/SearchResultViewModel.swift
+++ Challenge3/Challenge3/PresentationLayer/Modules/SearchView/SearchResult/SearchResultViewModel.swift
@@ -4,40 +4,4 @@
 
 import SwiftUI
 
-final class SearchResultViewModel: ObservableObject {
-    
-    func getScrollOpacity(geometry: GeometryProxy) -> Double {
-        let maxY = UIScreen.main.bounds.height
-        
-        // Координата верхней границы контейнера
-        let currentY = geometry.frame(in: .global).minY
-
-        let opacity: Double
-        
-        //координата, с которой начинает убывать прозрачнотсь
-        let yInitial = 0.9*maxY
-        let yInitial2 = 0.2*maxY
-        
-        // координата, на которой прозрачность становится нулевой
-        let yFinal = maxY
-        let yFinal2 = 0.05*maxY
-        
-        // угловой коэффициент линейной зависимости
-        let k = 1 / (yInitial - yFinal)
-        let kTop = 1 / (yInitial2 - yFinal2)
-        
-        // свободный коэффициент в линейной зависимости
-        let b = -k*yFinal
-        let bTop = -kTop*yFinal2
-        
-        if currentY < yInitial && currentY > yInitial2 {
-            opacity = 1
-        } else if currentY >= yInitial {
-            opacity = k * currentY + b
-        } else  {
-            opacity = kTop * currentY + bTop
-        }
-            
-        return opacity
-    }
-}
+final class SearchResultViewModel: ObservableObject {}
